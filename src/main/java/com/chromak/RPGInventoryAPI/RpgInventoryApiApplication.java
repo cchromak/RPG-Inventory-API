092314ed8d7167947b51bf8d66be15e8c59a94d4
Chris Chromak <cchromak@gmail.com>
2022-04-08 12:24:06 -0400

    Added Spring boot start project.

---
+++ src/main/java/com/chromak/RPGInventoryAPI/RpgInventoryApiApplication.java
@@ -0,0 +1,13 @@
+package com.chromak.RPGInventoryAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgInventoryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpgInventoryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chromak/RPGInventoryAPI/RpgInventoryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chromak.RPGInventoryAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgInventoryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
